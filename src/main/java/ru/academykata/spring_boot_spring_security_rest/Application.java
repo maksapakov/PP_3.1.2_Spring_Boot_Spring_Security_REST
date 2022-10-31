a6b62a63e4052d91c47cdba1256cb04362c36a4a
Макс Апаков <maksapakov@gmail.com>
2022-10-31 11:14:13 +0300

    Java pre-project. Практическая задача 3.1.2 Начало

---
+++ src/main/java/ru/academykata/spring_boot_spring_security_rest/Application.java
@@ -0,0 +1,13 @@
+package ru.academykata.spring_boot_spring_security_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/academykata/spring_boot_spring_security_rest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.academykata.spring_boot_spring_security_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
